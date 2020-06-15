5d6c19cb299e19b5f08432f00427e242bf3f14ea
David Guerra <david.guerra@ingenio-se.com>
2020-06-15 13:16:11 -0500

    solucion david guerra C4

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
-# Resolve the problem!!
+import re
+import string
+
 
 
 def run():
-    # Start coding here
+    
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        message= f.readline()
+    
+    
+    hidden=""   
+    for f in re.findall("([a-z]+)", message):
+        hidden += f
+    
+    print("The hidden message is :",hidden)
 
-
+    
 if __name__ == '__main__':
     run()
